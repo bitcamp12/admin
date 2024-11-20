00dd20d8c12ba014fab5c6c098f1e03d30abd287
unknown <j3415879@naver.com>
2024-11-20 12:16:19 +0900

    npc설정

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -16,4 +16,34 @@ public class AdminController {
     	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
         return "admin/index";  // index.html 템플릿을 렌더링
     }
+    
+    @GetMapping("/memberList")
+	public String memberList(Model model) {
+    	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
+        return "admin/body/memberList";  // index.html 템플릿을 렌더링
+    }
+    
+    @GetMapping("/sellerList")
+ 	public String sellerList(Model model) {
+     	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
+         return "admin/body/sellerList";  // index.html 템플릿을 렌더링
+     }
+    
+    @GetMapping("/noticeList")
+ 	public String noticeList(Model model) {
+     	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
+         return "admin/body/noticeList";  // index.html 템플릿을 렌더링
+     }
+    
+    @GetMapping("/noticeDetail")
+ 	public String noticeDetail(Model model) {
+     	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
+         return "admin/body/noticeDetail";  // index.html 템플릿을 렌더링
+     }
+    
+    @GetMapping("/noticeWriteForm")
+ 	public String noticeWriteForm(Model model) {
+     	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
+         return "admin/body/noticeWriteForm";  // index.html 템플릿을 렌더링
+     }
 }
