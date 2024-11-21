f1a0c20b2f56fdd88a24c2d82e7130724379acd2
unknown <j3415879@naver.com>
2024-11-21 14:43:12 +0900

    feautre(리뷰):리뷰관리 html

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -52,4 +52,16 @@ public class AdminController {
       	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
           return "admin/body/noticeUpdateForm";  // index.html 템플릿을 렌더링
       }
+    
+    @GetMapping("/reviewAfterList")
+  	public String reviewAfterList(Model model) {
+      	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
+          return "admin/body/reviewAfterList";  // index.html 템플릿을 렌더링
+      }
+    
+    @GetMapping("/reviewBeforeList")
+  	public String reviewBeforeList(Model model) {
+      	//관리자 정보 추후 추가하기 model 어트리뷰트..//	
+          return "admin/body/reviewBeforeList";  // index.html 템플릿을 렌더링
+      }
 }
