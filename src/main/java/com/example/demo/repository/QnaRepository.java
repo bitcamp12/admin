2167a78ec805be0c0a1e4d6783ab0f661be0e249
unknown <j3415879@naver.com>
2024-12-09 16:32:08 +0900

    페이징 끝

--- src/main/java/com/example/demo/repository/QnaRepository.java
+++ src/main/java/com/example/demo/repository/QnaRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Qna;
@@ -13,7 +14,15 @@ import com.example.demo.entity.Qna;
 public interface QnaRepository extends JpaRepository<Qna, Integer> {
  // 필요한 경우, 커스텀 쿼리 메서드 선언
 	
-	@Query("SELECT q FROM Qna q LEFT JOIN q.reply r")
-	List<Qna> findAllWithReply();
+	  // LEFT JOIN을 사용한 Qna와 Reply 페이징
+    @Query("SELECT q FROM Qna q LEFT JOIN q.reply r")
+    Page<Qna> findAllWithReply(Pageable pageable);
+
+	long countByTitleContaining(String value);
+
+	Page<Qna> findByTitleContaining(String value, Pageable pageable);
+
+	@Query("SELECT q FROM Qna q LEFT JOIN q.reply r where q.title like %:value% ")
+	Page<Qna> findAllWithReplyKeyword(Pageable pageable, @Param(value = "value") String value);
 
 }
--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -1,16 +1,5 @@
 var bookSeq = "";
 
-function memberPaging(page){
-	if($('#value').val() == '')
-		location.href = "/member/list?page=" + (page-1);
-	else {
-		//let columnName = $('#columnName').val();
-		//let value = $('#value').val();
-		//location.href = "/member/getSearchList?columnName="+columnName+"&value="+value+"&page=" + (page-1);
-		$('#page').val(page-1);
-		$('#searchListBtn').trigger('click');
-	}
-}
 
 function showModal(message) {
 	document.getElementById('modalMessage').textContent = message;
