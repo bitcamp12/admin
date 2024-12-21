affaeb6b2f1f9d90391156e50746bde4745f7967
GunWooJung <51355208+GunWooJung@users.noreply.github.com>
2024-12-21 20:38:24 +0900

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 허용
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080") // 허용할 도메인
+                .allowedOrigins("http://223.130.153.17:8080", "http://localhost:8080") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 포함 여부
