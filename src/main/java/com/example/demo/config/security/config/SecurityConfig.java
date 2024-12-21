b1a9f63f468c6a796ef55a2e67b301c001bdb0c8
unknown <j3415879@naver.com>
2024-12-21 22:44:33 +0900

    cors

--- src/main/java/com/example/demo/config/security/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/security/config/SecurityConfig.java
@@ -42,14 +42,7 @@ public class SecurityConfig {
 //    @Lazy
 //    private SellerAuthenticationProvider sellerAuthenticationProvider;
     
-    public void addCorsMappings(CorsRegistry registry) {
-        // 특정 경로에 대한 CORS 설정
-        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:8080", "http://223.130.153.17:8080") // 허용할 오리진
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 자격 증명 포함 허용
-    }
+
 
     @Bean 
     public PasswordEncoder passwordEncoder() {
